4b03f7d74b937a9195e319d4e53498b9d850756f
denzero13 <46201325+denzero13@users.noreply.github.com>
2021-10-27 16:13:25 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 tmp = "data/tmp.csv"
 devices = "data/devices.json"
 log = "data/log.csv"
-ip_network_diapason = "172.16.0.0/23"
+ip_network_diapason = "YOUR NETWORK DIAPASON"
 visual_for_terminal = "data/visual.csv"
